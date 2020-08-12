6fc48ec9d90069b903ef4c295e653d2422912c57
chengyuying <chengyuying@thunisoft.com>
2020-08-12 13:55:10 +0800

    My second change on branch1.

--- gitTest/src/Hello.java
+++ gitTest/src/Hello.java
@@ -4,9 +4,14 @@ public class Hello {
         System.out.println("Hello,"+str);
     }
 
+    public static String test1(String str){
+        String s = "This is test1, the parameter is "+str;
+        return s;
+    }
     public static void main(String[] args){
         printHello("this is my first change on branch3.");
         printHello("this is my second change on branch3.");
         printHello("this is my first change on branch4.");
+
     }
 }
